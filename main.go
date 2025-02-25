2dba226a6e9139d67e43e275b166dbd48abd7160
josiah-mbao <josiahmbaomc@gmail.com>
2025-02-25 20:13:13 +0300

    Restructure codebase

--- main.go
+++ main.go
@@ -3,17 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"reverse-proxy/routes" // Replace with your module name
 )
 
-func main() {
-	// Initialize routes
-	router := routes.InitializeRoutes()
+func server() {
+	// Serves dynamic content
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "The server that served you this page was built by Josiah, using Go.")
+	})
+
+	// Serve static files from folder called "static"
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static", http.StripPrefix("/static", fs))
+
 
 	// Start the server
-	port := ":8080"
-	fmt.Printf("Reverse Proxy Server is running on http://localhost%s\n", port)
-	if err := http.ListenAndServe(port, router); err != nil {
-		panic(err)
-	}
+	fmt.Println("Server running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
