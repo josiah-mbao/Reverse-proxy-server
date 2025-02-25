19772e0606a1e3ef8813522561e41f85756cead8
josiah-mbao <josiahmbaomc@gmail.com>
2025-02-25 20:58:01 +0300

    Getting codebase ready for containerization

--- main.go
+++ main.go
@@ -3,23 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
-func server() {
-	// Serves dynamic content
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "The server that served you this page was built by Josiah, using Go.")
-	})
-
-	// Serve static files from folder called "static"
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static", http.StripPrefix("/static", fs))
-
-	// Start the server
-	fmt.Println("Server running on http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+func reverseProxy(target string) http.Handler {
+	targetURL, err := url.Parse(target)
+	if err != nil {
+		fmt.Println("Error parsing URL: ", err)
+		return nil
+	}
+	return httputil.NewSingleHostReverseProxy(targetURL)
 }
 
 func main() {
-	server()
+	backend := "https://jsonplaceholder.typicode.com"
+	http.Handle("/", reverseProxy(backend))
+	fmt.Println("Aight, starting server on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Damn, the server failed to start:", err)
+	}
 }
